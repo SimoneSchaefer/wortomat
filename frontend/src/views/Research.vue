6cead30648c0a544c58781a298bbcf8673acf5ac
Simone Schäfer <imkreislaufen@googlemail.com>
2021-09-01 22:23:44 +0200

    display research list

--- frontend/src/views/Research.vue
+++ frontend/src/views/Research.vue
@@ -1,7 +1,7 @@
 <template>
 <SplitView stateKey="research">
   <template v-slot:leftpanel>
-    RESEARCH
+    <ResearchList></ResearchList>
   </template>
   <template v-slot:content>
     RRRRESEARCH
@@ -15,6 +15,7 @@ import { Options, Vue } from 'vue-class-component';
 import EditableLabel from '@/components/shared/inline-edit/EditableLabel.vue';
 import FilterMenu from '@/components/shared/filter/FilterMenu.vue';
 import ResearchMenu from '@/components/research/ResearchMenu.vue';
+import ResearchList from '@/components/research/ResearchList.vue';
 import SplitView from './SplitView.vue';
 import { NOVEL_ITEM_KEYS } from '@/store/keys';
 
@@ -23,6 +24,7 @@ import { NOVEL_ITEM_KEYS } from '@/store/keys';
   components: { 
     EditableLabel, 
     ResearchMenu,
+    ResearchList,
     FilterMenu,
     SplitView
   }
