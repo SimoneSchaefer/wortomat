37686463c039e740f1e4c5f580eec5451f316aa6
Simone Schäfer <imkreislaufen@googlemail.com>
2021-09-01 19:49:20 +0200

    extract splitView component

--- frontend/src/views/Research.vue
+++ frontend/src/views/Research.vue
@@ -1,35 +1,46 @@
 <template>
-<SplitView stateKey="chapters">
-  <template v-slot:leftpanel>
-    <FilterMenu></FilterMenu>
-    <ChapterList></ChapterList>
-  </template>
-  <template v-slot:content>
-    <SelectedChapters></SelectedChapters>
-  </template>
-</SplitView>
-<ChapterMenu></ChapterMenu>
+  <Splitter style="height: 100%" stateKey="chapters-splitter">
+    <SplitterPanel>
+      <ScrollPanel>        
+        <FilterMenu></FilterMenu>
+        RESEARCH!
+      </ScrollPanel>
+    </SplitterPanel>
+    <SplitterPanel class="bg-gray">
+      <ScrollPanel style="height: 100%">
+        SELECTED RESEARCH!
+      </ScrollPanel>
+    </SplitterPanel>
+  </Splitter>
+  <ChapterMenu></ChapterMenu>
 </template>
 
+
+
+
 <script lang="ts">
+import ScrollPanel from 'primevue/scrollpanel';
+import Splitter from 'primevue/splitter';
+import SplitterPanel from 'primevue/splitterpanel';
 import { Options, Vue } from 'vue-class-component';
 import EditableLabel from '@/components/shared/inline-edit/EditableLabel.vue';
 import FilterMenu from '@/components/shared/filter/FilterMenu.vue';
 import ChapterList from '@/components/chapters/ChapterList.vue';
 import ChapterMenu from '@/components/chapters/ChapterMenu.vue';
 import SelectedChapters from '@/components/chapters/SelectedChapters.vue';
-import SplitView from './SplitView.vue';
 import { NOVEL_ITEM_KEYS } from '@/store/keys';
 
 
 @Options({
   components: { 
+    Splitter, 
+    SplitterPanel, 
+    ScrollPanel,
     EditableLabel, 
     ChapterList, 
     SelectedChapters, 
     ChapterMenu,
-    FilterMenu,
-    SplitView
+    FilterMenu
   }
 })
 export default class Chapters extends Vue {
---
+++ frontend/src/views/SplitView.vue
@@ -0,0 +1,33 @@
+<template>
+  <Splitter style="height: 100%" :stateKey="stateKey">
+    <SplitterPanel>
+      <ScrollPanel>        
+        <slot name="leftpanel"></slot>
+      </ScrollPanel>
+    </SplitterPanel>
+    <SplitterPanel class="bg-gray">
+      <ScrollPanel style="height: 100%">
+        <slot name="content"></slot>
+      </ScrollPanel>
+    </SplitterPanel>
+  </Splitter>
+</template>
+
+<script lang="ts">
+import ScrollPanel from 'primevue/scrollpanel';
+import Splitter from 'primevue/splitter';
+import SplitterPanel from 'primevue/splitterpanel';
+import { Options, Vue } from 'vue-class-component';
+import { Prop } from 'vue-property-decorator';
+
+@Options({
+  components: { 
+    Splitter, 
+    SplitterPanel, 
+    ScrollPanel
+  }
+})
+export default class SplitView extends Vue {
+  @Prop() stateKey: string;
+}
+</script>
