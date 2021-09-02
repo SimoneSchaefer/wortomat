141de1937e5122427eae82f12477fdc68885fc06
Simone Schäfer <imkreislaufen@googlemail.com>
2021-09-02 19:47:37 +0200

    remove about router path

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -23,14 +23,6 @@ const routes: Array<RouteRecordRaw> = [
         path: 'export',
         component: () => import(/* webpackChunkName: "about" */ '../views/Export.vue'),
       }]
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Export.vue')
   }
 ]
 
