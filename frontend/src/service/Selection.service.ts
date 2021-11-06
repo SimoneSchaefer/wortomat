5353253f97fc2489e541879dc9859699b186481e
Simone Schäfer <imkreislaufen@googlemail.com>
2021-11-06 20:52:15 +0100

    fix error if there is no parent with child

--- frontend/src/service/Selection.service.ts
+++ frontend/src/service/Selection.service.ts
@@ -5,19 +5,22 @@ import { KEY_TO_CHILD } from "@/store/store-api-adapter";
 
 export class SelectionService {
     getFirstItemToSelect(state: IState, key: NOVEL_ITEM_KEYS, loadedItems: BaseModel[]) {
-    const keyOfInterest = KEY_TO_CHILD.has(key) ? KEY_TO_CHILD.get(key) : key;
-    const selection = state.selection.get(keyOfInterest) || [];
-    let itemToSelect = null;
-    if (!selection.length && loadedItems.length) {
-      if (!KEY_TO_CHILD.has(key)) {
-        itemToSelect = loadedItems[0];
-      } else {
-        const children = loadedItems[0][KEY_TO_CHILD.get(key)];
-        if (children?.length) {
-          itemToSelect = children[0];
+        const keyOfInterest = KEY_TO_CHILD.has(key) ? KEY_TO_CHILD.get(key) : key;
+        const selection = state.selection.get(keyOfInterest) || [];
+        let itemToSelect = null;
+        if (!selection.length && loadedItems.length) {
+            if (!KEY_TO_CHILD.has(key)) {
+                itemToSelect = loadedItems[0];
+            } else {
+                const firstParentWithChildren = loadedItems.find(item => item[KEY_TO_CHILD.get(key)]?.length)
+                if (firstParentWithChildren) {
+                    const children = firstParentWithChildren[KEY_TO_CHILD.get(key)];
+                    if (children?.length) {
+                        itemToSelect = children[0];
+                    }
+                }                
+            }
         }
-      }
+        return itemToSelect;    
     }
-    return itemToSelect;    
-  }
 }
