6d781580486b85322731fca16e3f4b6c2d7ef023
Simone Schäfer <imkreislaufen@googlemail.com>
2021-12-11 14:21:31 +0100

    feat(#50): add tooltips to editor buttons

--- frontend/src/components/shared/wysisyg/TipTap.vue
+++ frontend/src/components/shared/wysisyg/TipTap.vue
@@ -2,24 +2,25 @@
     <div class="editor" v-if="editor">
         <div id="editor-toolbar">
             <div class="tools">
-                <button @click="runCommand('toggleBold')" :class="{ 'is-active': editor.isActive('bold') }"><i class="fa fa-bold"></i></button>
-                <button @click="runCommand('toggleItalic')" :class="{ 'is-active': editor.isActive('italic') }"><i class="fa fa-italic"></i></button>
-                <button @click="runCommand('toggleStrike')" :class="{ 'is-active': editor.isActive('strike') }"><i class="fa fa-strikethrough"></i></button>
-                <button @click="runCommand('toggleCode')" :class="{ 'is-active': editor.isActive('code') }"><i class="fa fa-code"></i></button>
-                <button @click="runCommand('toggleHeading', { level: 1})" :class="{ 'is-active': editor.isActive('heading', { level: 1 }) }"><i class="fa fa-heading"></i></button>
-                <button @click="runCommand('toggleBulletList')" :class="{ 'is-active': editor.isActive('bulletList') }"><i class="fa fa-list-ul"></i></button>
-                <button @click="runCommand('toggleOrderedList')" :class="{ 'is-active': editor.isActive('orderedList') }"><i class="fa fa-list-ol"></i></button>
-                <button @click="runCommand('toggleBlockquote')" :class="{ 'is-active': editor.isActive('blockquote') }"><i class="fa fa-quote-right"></i></button>
-                <button @click="runCommand('toggleHighlight', markerColors['idea'])" :class="{ 'is-active': editor.isActive('TodoMarker', markerColors['idea'])}"><i class="fa fa-lightbulb"></i></button>
-                <button @click="runCommand('toggleHighlight', markerColors['fixme'])" :class="{ 'is-active': editor.isActive('TodoMarker', markerColors['fixme'])}"><i class="fa fa-band-aid"></i></button>
-                <button @click="runCommand('toggleHighlight', markerColors['todo'])" :class="{ 'is-active': editor.isActive('TodoMarker', markerColors['todo'])}"><i class="fa fa-clipboard-list"></i></button>
+                <button class="padding-left editor-button" :title="$t('editor.bold')" @click="runCommand('toggleBold')" :class="{ 'is-active': editor.isActive('bold') }"><i class="fa fa-bold"></i></button>
+                <button class="editor-button" :title="$t('editor.italic')" @click="runCommand('toggleItalic')" :class="{ 'is-active': editor.isActive('italic') }"><i class="fa fa-italic"></i></button>
+                <button class="editor-button" :title="$t('editor.strike')" @click="runCommand('toggleStrike')" :class="{ 'is-active': editor.isActive('strike') }"><i class="fa fa-strikethrough"></i></button>
+                <button class="editor-button" :title="$t('editor.code')" @click="runCommand('toggleCode')" :class="{ 'is-active': editor.isActive('code') }"><i class="fa fa-code"></i></button>
+                <button class="editor-button" :title="$t('editor.heading')" @click="runCommand('toggleHeading', { level: 1})" :class="{ 'is-active': editor.isActive('heading', { level: 1 }) }"><i class="fa fa-heading"></i></button>
+                <button class="editor-button"  :title="$t('editor.ul')" @click="runCommand('toggleBulletList')" :class="{ 'is-active': editor.isActive('bulletList') }"><i class="fa fa-list-ul"></i></button>
+                <button class="editor-button" :title="$t('editor.ol')" @click="runCommand('toggleOrderedList')" :class="{ 'is-active': editor.isActive('orderedList') }"><i class="fa fa-list-ol"></i></button>
+                <button class="editor-button" :title="$t('editor.quote')" @click="runCommand('toggleBlockquote')" :class="{ 'is-active': editor.isActive('blockquote') }"><i class="fa fa-quote-right"></i></button>
+                <button class="editor-button padding-left" :title="$t('editor.highlight_idea')" @click="runCommand('toggleHighlight', markerColors['idea'])" :class="{ 'is-active': editor.isActive('TodoMarker', markerColors['idea'])}"><i class="fa fa-lightbulb"></i></button>
+                <button class="editor-button" :title="$t('editor.highlight_todo')" @click="runCommand('toggleHighlight', markerColors['todo'])" :class="{ 'is-active': editor.isActive('TodoMarker', markerColors['todo'])}"><i class="fa fa-clipboard-list"></i></button>
+                <button class="editor-button" :title="$t('editor.highlight_fixme')" @click="runCommand('toggleHighlight', markerColors['fixme'])" :class="{ 'is-active': editor.isActive('TodoMarker', markerColors['fixme'])}"><i class="fa fa-band-aid"></i></button>
+
+                <button class="editor-button padding-left" :title="$t('editor.clear')" @click="runCommand('unsetAllMarks') && this.runCommand('clearNodes')"><i class="fa fa-eraser"></i></button>
+                <button class="editor-button"  :title="$t('editor.undo')" @click="runCommand('undo')"><i class="fa fa-undo"></i></button>
+                <button class="editor-button" :title="$t('editor.redo')" @click="runCommand('redo')" ><i class="fa fa-redo"></i></button>
             </div>
             <div class="other">
-                <button @click="runCommand('unsetAllMarks') && this.runCommand('clearNodes')"><i class="fa fa-eraser"></i></button>
-                <button @click="runCommand('undo')"><i class="fa fa-undo"></i></button>
-                <button @click="runCommand('redo')" ><i class="fa fa-redo"></i></button>
-                <button @click="cancel"><i class="fa fa-times"></i></button>
-                <button @click="save" class="last"><i class="fa fa-save"></i></button>
+                <AppButton color="success" icon="fa fa-check" title="editor.save" v-on:click="save"></AppButton>            
+                <AppButton color="danger" icon="fa fa-times" title="editor.cancel" v-on:click="cancel"></AppButton>   
             </div>
         </div>
         <div class="editor-content">
@@ -35,10 +36,11 @@ import { Editor, EditorContent } from '@tiptap/vue-3'
 import StarterKit from '@tiptap/starter-kit'
 
 import TodoMarker from './todo-marker'
+import AppButton from '@/components/shared/Button.vue'
 
 
 @Options({
-  components: { EditorContent },
+  components: { EditorContent, AppButton },
   emits: [ 'save', 'cancel']
 })
 export default class TipTap extends Vue { 
@@ -98,9 +100,10 @@ export default class TipTap extends Vue {
 }
 #editor-toolbar {
     border: 1px solid #495057e0;
-    height:40px;
+    height:4em;
     display: flex;
     justify-content: space-between;
+    align-items: center;
     position: fixed;
 
     align-self: flex-start;
@@ -115,28 +118,35 @@ export default class TipTap extends Vue {
     display: flex;
 }
 
-#editor-toolbar .other button:first-child {
-    border-left: 1px solid #495057e0;
+#editor-toolbar .other {
+    margin-right: 2em;
+}
+.other {
+    border: 2px solid #1d1d1d;
 }
 
+.other button {
+    width: 3em !important;
+    border-left: 1px solid #1d1d1d !important;
+    border-right: 1px solid #1d1d1d !important;
+}
 
 #editor-toolbar .p-menubar {
     background: none;
 }
 
-#editor-toolbar button {
-    border: 0;
+#editor-toolbar button.editor-button {
     outline: none;
     color: #cececee0;
     background: var(--dark-background);
+    height: 4em;
     width: 4em;
+    border:none;
+
 }
 
-#editor-toolbar button:not(.last) {
-    border-right: 1px solid #cececee0;
-}
 
-#editor-toolbar button.is-active {
+#editor-toolbar button.editor-button.is-active {
     color: #6ba6e0;
 }
 
@@ -149,20 +159,32 @@ export default class TipTap extends Vue {
     border-right: none;
 }
 
+.padding-left {
+    margin-left: 3em;
+}
 
-#editor-toolbar .button {
+
+#editor-toolbar .button.editor-button {
     background: none;
     border: none;
     cursor: pointer;
     display: inline-block;
     padding: 3px 5px;
     width: 28px;
-    color: #495057e0;
+    color: #777676;
 }
 
-#editor-toolbar .button.is-active {
+#editor-toolbar button.editor-button:hover {
+    cursor: pointer;
+    color: #cecece;
+    background-color: #5f5f5f;
+}
+
+#editor-toolbar button.editor-button.is-active {
     color: #5daaf6;
 } 
+
+
 
 .editor-content {
     flex-grow: 1;
