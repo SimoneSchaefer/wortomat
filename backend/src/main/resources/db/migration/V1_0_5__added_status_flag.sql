72e5c6a270a2cfa47591033b122cf6bb1e2b9e6c
Simone Schäfer <imkreislaufen@googlemail.com>
2022-12-30 11:14:29 +0100

    actual migration

--- backend/src/main/resources/db/migration/V1_0_5__added_status_flag.sql
+++ backend/src/main/resources/db/migration/V1_0_5__added_status_flag.sql
@@ -1,4 +1,3 @@
-ALTER TABLE chapter ADD COLUMN plotline_id integer;
 ALTER TABLE chapter ADD COLUMN status integer;
 ALTER TABLE location ADD COLUMN status integer;
 ALTER TABLE research ADD COLUMN status integer;
